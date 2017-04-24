f7c11881b22a5326a509101e8bda08a8656a78d0
cbFowler <cbf25@txstate.edu>
2017-04-23 23:30:46 -0500

    New Test
    Added a test to TestGreeter.java - should pass successfully.

--- src/test/java/hello/TestGreeter.java
+++ src/test/java/hello/TestGreeter.java
@@ -20,11 +20,11 @@ public class TestGreeter {
 
    private Greeter g;
 
-	
+
 
    @Before
 
-   public void setUp() throws Exception 
+   public void setUp() throws Exception
 
    {
 
@@ -36,7 +36,7 @@ public class TestGreeter {
 
    @Test
 
-   public void testGreeterEmpty() 
+   public void testGreeterEmpty()
 
    {
 
@@ -46,11 +46,22 @@ public class TestGreeter {
 
    }
 
-	
 
    @Test
 
-   public void testGreeter() 
+   public void newtestcbfGreeterPass()
+
+   {
+      g.setName("Vladmir");
+      assertEquals(g.getName(),"Vladmir");
+      assertEquals(g.sayHello(),"Hello Vladmir!");
+   }
+
+
+
+   @Test
+
+   public void testGreeter()
 
    {
 
