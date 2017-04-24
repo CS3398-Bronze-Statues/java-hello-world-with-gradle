080c634ce2f24250e45f1dd49c5b70a136da3add
mcs152 <mcs152@txstate.edu>
2017-04-23 22:39:54 -0500

    Update TestGreeter.java

--- src/test/java/hello/TestGreeter.java
+++ src/test/java/hello/TestGreeter.java
@@ -1,15 +1,16 @@
 package hello;
-
 import static org.junit.Assert.*;
-
 import org.junit.Before;
 import org.junit.Test;
 
-
 public class TestGreeter {
-
    private Greeter g;
-	
+   
+   @Before
+   public void setUp() throws Exception 
+   {
+      g = new Greeter();
+   }
 
    @Test
    public void testGreeterEmpty() 
@@ -17,12 +18,12 @@ public class TestGreeter {
       assertEquals(g.getName(),"");
       assertEquals(g.sayHello(),"Hello!");
    }
-	
+
    @Test
-   public void testGreeter() 
-   {
-      g.setName("World");
-      assertEquals(g.getName(),"World");
-      assertEquals(g.sayHello(),"Hello World!");
-   }
+    public void testGreeter()    
+    {
+        g.setName("World");
+        assertEquals(g.getName(),"World");
+        assertEquals(g.sayHello(),"Hello World!");
+     }
 }
